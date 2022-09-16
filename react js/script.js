c89c28b1d89d4c869db44c5a03b2b8096c082572
akashsoni0786 <akashsoni@cedcoss.com>
2022-09-16 13:12:08 +0530

    Done

--- react js/script.js
+++ react js/script.js
@@ -7,6 +7,47 @@ let Employee = {};
 let Food = {};
 let emparr = [];
 var mode = "off";
+
+// Task 1 Add Details
+function addDetails() {
+  Employee = Object.create(emp);
+
+  var pos = document.getElementById("position");
+  var selectedpos = pos.options[pos.selectedIndex].text;
+
+  var typ = document.getElementById("type");
+  var selectedtyp = typ.options[typ.selectedIndex].text;
+
+  var available = document.getElementById("available");
+  var selectedavailable = available.options[available.selectedIndex].text;
+
+  if (selectedpos == "" || selectedtyp == "" || selectedavailable == "") {
+    alert("Please select from all category!");
+  } else {
+    Employee["position"] = selectedpos;
+    Employee["type"] = selectedtyp;
+    Employee["isAvailable"] = selectedavailable;
+    var row =
+      "<table border=1><tr><th>Position</th><th>Type</th><th>Availability</th></tr><tr><td>" +
+      Employee["position"] +
+      "</td><td>" +
+      Employee["type"] +
+      "</td><td>" +
+      Employee["isAvailable"] +
+      "</td></tr></table>";
+    document.getElementById("employeeadded").innerHTML = row;
+    showkeyvalues();
+    foodobj();
+  }
+}
+// Task 2 Showing Keys
+function showKeys() {
+  var keys = [String(Object.keys(Employee))];
+  emparr = Object.keys(Employee);
+  document.getElementById("keys").innerHTML =
+    "Total Keys : " + emparr.length + "     [" + keys + "]";
+}
+// Task 3 Showing details
 function showDetails() {
   Employee = Object.create(emp);
 
@@ -38,13 +79,7 @@ function showDetails() {
     foodobj();
   }
 }
-
-function showKeys() {
-  var keys = [String(Object.keys(Employee))];
-  emparr = Object.keys(Employee);
-  document.getElementById("keys").innerHTML =
-    "Total Keys : " + emparr.length + "     [" + keys + "]";
-}
+// Task 2 Showing Keys
 function showkeyvalues() {
   var row = Object.entries(Employee);
   let v = "[";
@@ -54,6 +89,7 @@ function showkeyvalues() {
   v += "]";
   document.getElementById("Keyvalue").innerHTML = v;
 }
+// Task 5 Merging of Objects
 function foodobj() {
   var Food = {
     resturant: "Brijwasi Resturant",
@@ -62,22 +98,23 @@ function foodobj() {
   };
   var merged = Object.assign(Food, Employee);
   var all = Object.entries(merged);
-  let v = "<h2>Merged Food and Employee</h2><br><table border=1>";
+  let v = "<h2>Merged Food and Employee Objects</h2><br><table border=1>";
   all.map((i) => {
     v += "<tr><td>" + i[0] + "</td><td>" + i[1] + "</td></tr>";
   });
   v += "</table>";
   document.getElementById("mergedarr").innerHTML = v;
 }
+// Task 6 prevent change in details
 function prevent() {
-    mode = "on";
+  mode = "on";
   Object.freeze(Employee);
-  alert("Prevent mode is on!")
+  alert("Prevent mode is on!");
 }
 function update() {
-    if(mode == "on"){
-        alert("Can't update due to prevent mode")
-    }
+  if (mode == "on") {
+    alert("Can't update due to prevent mode");
+  }
   var typr = document.getElementById("updatedtype");
   var selectedtypr = typr.options[typr.selectedIndex].text;
 
