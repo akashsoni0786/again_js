2e0036cfaa463a394940d438728eb22ce7abcb8d
akashsoni0786 <akashsoni@cedcoss.com>
2022-09-16 18:55:02 +0530

    Done

--- react js/script.js
+++ react js/script.js
@@ -2,6 +2,18 @@ const emp = {
   position: String,
   type: String,
   isAvailable: Boolean,
+  showDetails:function(){
+    var row =
+      "<table border=1><tr><th>Position</th><th>Type</th><th>Availability</th></tr><tr><td>" +
+      Employee["position"] +
+      "</td><td>" +
+      Employee["type"] +
+      "</td><td>" +
+      Employee["isAvailable"] +
+      "</td></tr></table>";
+    document.getElementById("employeetable").innerHTML = row;
+  }
+
 };
 let Employee = {};
 let Food = {};
@@ -49,35 +61,9 @@ function showKeys() {
 }
 // Task 3 Showing details
 function showDetails() {
-  Employee = Object.create(emp);
-
-  var pos = document.getElementById("position");
-  var selectedpos = pos.options[pos.selectedIndex].text;
-
-  var typ = document.getElementById("type");
-  var selectedtyp = typ.options[typ.selectedIndex].text;
-
-  var available = document.getElementById("available");
-  var selectedavailable = available.options[available.selectedIndex].text;
-
-  if (selectedpos == "" || selectedtyp == "" || selectedavailable == "") {
-    alert("Please select from all category!");
-  } else {
-    Employee["position"] = selectedpos;
-    Employee["type"] = selectedtyp;
-    Employee["isAvailable"] = selectedavailable;
-    var row =
-      "<table border=1><tr><th>Position</th><th>Type</th><th>Availability</th></tr><tr><td>" +
-      Employee["position"] +
-      "</td><td>" +
-      Employee["type"] +
-      "</td><td>" +
-      Employee["isAvailable"] +
-      "</td></tr></table>";
-    document.getElementById("employeetable").innerHTML = row;
+    Employee.showDetails();
     showkeyvalues();
     foodobj();
-  }
 }
 // Task 2 Showing Keys
 function showkeyvalues() {
